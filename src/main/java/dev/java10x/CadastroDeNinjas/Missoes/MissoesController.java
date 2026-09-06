36b2be345a9797862b0e983a130028e75c81bd3d
Iran Camargo <iran.jr21@hotmail.com>
2026-09-06 00:11:44 -0300

    Modelagem do BD, NinjaModel e MissoesModel feitos, anotations criadas para relacionamento de tabelas

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,4 @@
-package dev.java10x.CadastroDeNinjas;
+package dev.java10x.CadastroDeNinjas.Missoes;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,12 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class NinjaController {
+public class MissoesController {
 
-    @GetMapping("/boasvindas")
-    public String boasVindas(){
-        return "Essa é minha primeira mensagem nessa rota";
+    @GetMapping("/Missões")
+
+        public String missoesMenssagem() {
+            return "Missões ninjas: ";
+
+        }
     }
 
-}
 
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,5 +1,7 @@
-package dev.java10x.CadastroDeNinjas;
+package dev.java10x.CadastroDeNinjas.Ninjas;
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+
 
 //JPA - Java Persistence API
 //Entity tranforma uma classe em uma Entidade do Banco de Dados
@@ -10,9 +12,17 @@ public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String email;
+
     private int idade;
+
+    //@ManyToOne Um ninja pode ter uma unica missão
+    @ManyToOne
+    @JoinColumn(name = "missoes_id") //Foreing KEY ou chave-estrangeira
+    private MissoesModel missoes;
 
     public NinjaModel() {
     }
