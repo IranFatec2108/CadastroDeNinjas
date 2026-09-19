50bf481feacf63e3c7a795537f8adda6e663e147
Iran Camargo <iran.jr21@hotmail.com>
2026-09-19 03:08:00 -0300

    feat: Inclui a rota de criação dos Ninjas usando requisição POST, com o Postman

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -3,9 +3,6 @@ import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
 import lombok.*;
 
-
-//JPA - Java Persistence API
-//Entity tranforma uma classe em uma Entidade do Banco de Dados
 @Entity
 @Table(name = "tb_cadastro")
 @NoArgsConstructor
@@ -32,11 +29,7 @@ public class NinjaModel {
 
     //@ManyToOne Um ninja pode ter uma unica missão
     @ManyToOne
-    @JoinColumn(name = "missoes_id") //Foreing KEY ou chave-estrangeira
+    @JoinColumn(name = "missoes_id") //Foreign KEY ou chave-estrangeira
     private MissoesModel missoes;
-
-    @Column(name = "rank")
-    private String rank;
-
 
 }
