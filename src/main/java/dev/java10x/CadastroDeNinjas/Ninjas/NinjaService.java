daed997b9caf18d1e3dce39b4b49ed9a9fba02c5
Iran Camargo <iran.jr21@hotmail.com>
2026-09-20 22:47:29 -0300

    feat: Inclui o metodo PUT, mas sem funcionamento adequado, sendo necessario alteração para atualização parcial dos ninjas

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -34,5 +34,12 @@ public class NinjaService {
       ninjaRepository.deleteById(id);
     }
 
+    // Alterar Ninja
+    public NinjaModel atualizarNinja(Long id, NinjaModel ninjaAtualizado){
+        if(ninjaRepository.existsById(id)){
+            ninjaRepository.save(ninjaAtualizado);
+        }
+        return null;
+    }
 
 }
